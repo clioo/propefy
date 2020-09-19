1fc10d5da5229171e9bd5e4f0007bfcc1fde14d3
clioo <jesus_acosta1996@hotmail.com>
2020-09-18 23:06:55 -0600

    changed smtp variables

--- app/propefy/settings.py
+++ app/propefy/settings.py
@@ -132,9 +132,9 @@ STATIC_ROOT = '/vol/web/static'
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'propefy.estate@gmail.com'
-EMAIL_HOST_PASSWORD = 'd#22wWzqAq1221aDDGhjK'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
 AUTH_USER_MODEL = 'core.User'
