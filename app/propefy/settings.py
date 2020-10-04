da296aaf466984ad548225d047d9638919fc2c7a
clioo <jesus_acosta1996@hotmail.com>
2020-10-03 23:19:29 -0600

    static dirs added

--- app/propefy/settings.py
+++ app/propefy/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'kaari/static')
+    os.path.join(BASE_DIR, 'propefy/static')
 ]
 
 
