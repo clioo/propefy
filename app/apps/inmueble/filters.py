7e97aa7345a78959cb2fb38508a0911fb690d9b3
carlos <jesus_acosta1996@hotmail.com>
2021-04-24 12:42:56 -0600

    Inmuebles filter implemented and working good now

--- app/apps/inmueble/filters.py
+++ app/apps/inmueble/filters.py
@@ -1,21 +1,23 @@
 from django_filters import rest_framework as filters
-from apps.core.models import Inmueble
+from apps.core.models import Inmueble, Municipio
 
 
 class InmuebleFilter(filters.FilterSet):
-    # tipo_inmueble
-    # ciudad
-    # estado
-    # categoria
-    # precio_min
-    # precio_max
-    # recamaras
-    precio_min = filters.NumberFilter(field_name='price', lookup_expr='gt')
+    recamaras = filters.NumberFilter(field_name='recamaras', lookup_expr='gte')
+    precio_min = filters.NumberFilter(field_name='precio', lookup_expr='gte')
+    precio_max = filters.NumberFilter(field_name='precio', lookup_expr='lte')
+    estado = filters.NumberFilter(field_name='municipio__estado')
     class Meta:
         model = Inmueble
-        fields = ('id', 'titulo', 'descripcion', 'dueno', 'acepta_credito',
+        fields = ('titulo', 'descripcion', 'dueno', 'acepta_credito',
                   'categoria', 'tipo_propiedad', 'municipio', 'precio',
                    'moneda', 'precio_periodo', 'status', 'estacionamientos',
                    'banos', 'medios_banos', 'direccion',
                    'latitud', 'longitud', 'creada', 'actualizada',
-                   'precio_min')
+                   'precio_min', 'precio_max', 'recamaras')
+
+
+class MunicipioFilter(filters.FilterSet):
+    class Meta:
+        model = Municipio
+        fields = ('estado',)
