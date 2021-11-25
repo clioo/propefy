8ae4750a0200fdddf8cbc4b36c9b7cc34c0cfecc
carlos <jesus_acosta1996@hotmail.com>
2021-11-24 21:06:09 -0700

    antiguedad, m2 and m_2_construccion added in range filters

--- app/apps/inmueble/filters.py
+++ app/apps/inmueble/filters.py
@@ -10,6 +10,14 @@ class InmuebleFilter(filters.FilterSet):
         lookup_expr='lte')
     precio_min = filters.NumberFilter(field_name='precio', lookup_expr='gte')
     precio_max = filters.NumberFilter(field_name='precio', lookup_expr='lte')
+    antiguedad_min = filters.NumberFilter(field_name='antiguedad', lookup_expr='gte')
+    antiguedad_max = filters.NumberFilter(field_name='antiguedad', lookup_expr='lte')
+    m_2_min = filters.NumberFilter(field_name='m_2', lookup_expr='gte')
+    m_2_max = filters.NumberFilter(field_name='m_2', lookup_expr='lte')
+    m_2_construccion_min = filters.NumberFilter(field_name='m_2_construccion',
+                                                lookup_expr='gte')
+    m_2_construccion_max = filters.NumberFilter(field_name='m_2_construccion',
+                                                lookup_expr='lte')
     estado = filters.NumberFilter(field_name='municipio__estado')
     full_text = filters.Filter(field_name='search_vector', lookup_expr='icontains')
     tipo_propiedad = ListFilter(field_name='tipo_propiedad_id')
@@ -21,7 +29,8 @@ class InmuebleFilter(filters.FilterSet):
                   'banos', 'medios_banos', 'direccion',
                   'latitud', 'longitud', 'creada', 'actualizada',
                   'precio_min', 'precio_max', 'recamaras', 'full_text',
-                  'dentro_de_privada', 'se_admiten_mascotas', 'amueblada')
+                  'dentro_de_privada', 'se_admiten_mascotas', 'amueblada',
+                  'antiguedad', 'm_2', 'm_2_construccion')
 
 
 class MunicipioFilter(filters.FilterSet):
