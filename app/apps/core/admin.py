e78d29f89136a8ef8bdef0cb8cbb2fbe5214ba8a
Carlos Acosta <jesus_acosta1996@hotmail.com>
2022-05-03 18:53:09 -0600

    adding groups to permissions part in admin

--- app/apps/core/admin.py
+++ app/apps/core/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
         (_('Personal info'), {'fields': ('name',)}),
         (
             _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')}
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
